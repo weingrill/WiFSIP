1aa955b95a3e571a2d46ca50ade78efb720a6a1c
hg <hg@localhost>
2014-11-11 16:55:44 +0100

    added index.html

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 scp ~/workspace/WiFSIP/src/nightshow.py sro@stella:/stella/home/sro/scripts/
 scp ~/workspace/WiFSIP/src/obslog.py sro@stella:/stella/home/sro/scripts/
+scp ~/workspace/WiFSIP/src/index.html sro@stella:/stella/home/sro/www/stella/graphs/
