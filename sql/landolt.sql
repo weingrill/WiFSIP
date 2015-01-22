a82781ba061d3327bbc65a309442a494f5aacd54
Joerg Weingrill <jweingrill@aip.de>
2015-01-22 17:33:58 +0100

    added sql for landolt

---
+++ sql/landolt.sql
@@ -0,0 +1,21 @@
+CREATE TABLE landolt(
+ name varchar(11),
+ ra double precision,
+ dec double precision,
+ vmag real,
+ bv real,
+ ub real,
+ vr real,
+ ri real,
+ vi real,
+ nobs integer,
+ nnig integer,
+ e_vmag real,
+ e_bv real,
+ e_ub real,
+ e_vr real,
+ e_ri real,
+ e_vi real,
+ PRIMARY KEY (name)
+);
+
