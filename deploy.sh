197c5b4348abbaab5f1725472a2b4a8daad950c3
Joerg Weingrill <jweingrill@aip.de>
2016-01-18 17:43:21 +0100

    added shebang to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 scp ~/workspace/WiFSIP/src/nightshow.py sro@stella:/stella/home/sro/scripts/
 scp ~/workspace/WiFSIP/src/obslog.py sro@stella:/stella/home/sro/scripts/
 scp ~/workspace/WiFSIP/src/index.html sro@stella:/stella/home/sro/www/stella/graphs/
